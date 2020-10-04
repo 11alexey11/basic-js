69447f3b1b06085859ebda727939399964731bd2
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-10-04 12:03:35 +0300

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {}) {
+  debugger;
+  let copyStr = String(str);
+  let repeatTimes = options.repeatTimes;
+  let separator = options.separator;
+  if (separator === undefined) separator = '+';
+  let addition = String(options.addition);
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+  if (additionSeparator === undefined) additionSeparator = '|';
+  if (repeatTimes === undefined) {
+    if (typeof(addition) === 'string') {
+      return copyStr + addition;
+    }
+    return copyStr;
+  } else if (typeof(repeatTimes) === 'number') {
+    if (additionRepeatTimes === undefined) {
+      copyStr = '';
+      for (let i = 0; i < repeatTimes; i++) {
+        if (i === repeatTimes - 1) {
+          copyStr += str;
+        } else {
+          copyStr += str + separator;
+        }
+      }
+    } else {
+      copyStr = '';
+      let additionCopy = '';
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        if (i === additionRepeatTimes - 1) {
+          additionCopy += addition;
+        } else {
+          if (additionSeparator !== '|') additionCopy += addition + additionSeparator;
+        }
+      }
+      for (let i = 0; i < repeatTimes; i++) {
+        if (i  === repeatTimes - 1) {
+          copyStr += str + additionCopy;
+        } else {
+          copyStr += str + additionCopy + separator;
+        }
+      }
+    }
+  }
+  return copyStr;
 };
   
