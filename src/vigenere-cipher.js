a04dadfd0da9050dbceffc9fec6ba0d37925b1b2
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-10-04 14:30:04 +0300

    feat: add VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,90 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.modification = modification;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.table = [];
+  }
+  createTable() {
+    let alphabetArr = this.alphabet.split('');
+    let index = 0;
+    let element = '';
+    while (index !== this.alphabet.length) {
+      this.table[index] = alphabetArr.slice();
+      element = alphabetArr.shift();
+      alphabetArr.push(element);
+      index++;
+    }
+  }
+  createKey(message, key) {
+    let newKey = '';
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        if (j < key.length) {
+          newKey += key[j];
+        }
+        else {
+          j = 0;
+          newKey += key[j];
+        }
+        j++;
+      } else {
+        newKey += message[i];
+      }
+    }
+    return newKey;
+  }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    this.createTable();
+    let newKey = this.createKey(message, key);
+    let encryptMessage = '';
+    let alphabetArr = this.alphabet.split('');
+    let firstIndex;
+    let secondIndex;
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        firstIndex = alphabetArr.indexOf(message[i]);
+        secondIndex = alphabetArr.indexOf(newKey[i]);
+        encryptMessage += this.table[firstIndex][secondIndex];
+      } else {
+        encryptMessage += message[i];
+      }
+    }
+    if (this.modification === false) return encryptMessage.split('').reverse().join('');
+    return encryptMessage;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw new Error();
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    this.createTable();
+    let newKey = this.createKey(message, key);
+    let encryptMessage = '';
+    let alphabetArr = this.alphabet.split('');
+    let firstIndex;
+    let secondIndex;
+    let addArr = [];
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
+        firstIndex = alphabetArr.indexOf(newKey[i]);
+        addArr = this.table[firstIndex];
+        secondIndex = addArr.indexOf(message[i]);
+        encryptMessage += alphabetArr[secondIndex];
+      } else {
+        encryptMessage += message[i];
+      }
+    }
+    if (this.modification === false) return encryptMessage.split('').reverse().join('');
+    return encryptMessage;
   }
 }
 
