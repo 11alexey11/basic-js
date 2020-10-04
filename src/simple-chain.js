12cf2eaa3e73c5df1b6cf6f41fd3e8d8a9bbb05b
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-10-04 08:50:06 +0300

    feat: add chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,75 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: '',
+  count: 0,
+  values: [],
+  flag: false,
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.values.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      if (this.chain === '') {
+        this.chain += `( )`;
+      } else {
+        this.chain += `~~( )`;
+      }
+      this.values.push('');
+    } else {
+      if (this.chain === '') {
+        this.chain += `( ${String(value)} )`;
+      } else {
+        this.chain += `~~( ${String(value)} )`;
+      }
+      this.values.push(String(value));
+    }
+    this.count = this.values.length;
+    if (this.flag) return this.chain;
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(position) !== 'number' || (position ^ 0) !== position 
+        || position > this.getLength() || position <= 0) {
+          this.chain = '';
+          this.count = 0;
+          this.values = [];
+          this.flag = false;
+          throw new Error();
+    } else {
+      this.chain = '';
+      this.count = 0;
+      let copyValues = this.values.slice();
+      this.values = [];
+      this.flag = true;
+      for (let i = 0; i < copyValues.length; i++) {
+        if (i !== position - 1) {
+          this.chain = this.addLink(copyValues[i]);
+        }
+      }
+      this.flag = false;
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.values.reverse();
+    this.chain = '';
+    let copyValues = this.values.slice();
+    this.values = [];
+    this.flag = true;
+    for (let i = 0; i < copyValues.length; i++) {
+      this.chain = this.addLink(copyValues[i]);
+    }
+    this.flag = false;
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.values = [];
+    this.count = 0;
+    let chain = this.chain;
+    this.chain = '';
+    this.flag = false;
+    return chain;
   }
 };
 
